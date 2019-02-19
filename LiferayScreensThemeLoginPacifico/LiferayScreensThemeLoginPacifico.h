4a8e328edc0a89452e69227ad0a279c091684e92
jhonly junior garcia pitoy <jhonly_thebest1@hotmail.com>
2019-02-19 16:23:48 -0500

    Initial Commit

---
+++ LiferayScreensThemeLoginPacifico/LiferayScreensThemeLoginPacifico.h
@@ -0,0 +1,19 @@
+//
+//  LiferayScreensThemeLoginPacifico.h
+//  LiferayScreensThemeLoginPacifico
+//
+//  Created by Everis on 19/02/19.
+//  Copyright © 2019 JJgarcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiferayScreensThemeLoginPacifico.
+FOUNDATION_EXPORT double LiferayScreensThemeLoginPacificoVersionNumber;
+
+//! Project version string for LiferayScreensThemeLoginPacifico.
+FOUNDATION_EXPORT const unsigned char LiferayScreensThemeLoginPacificoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiferayScreensThemeLoginPacifico/PublicHeader.h>
+
+
